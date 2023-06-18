8e325efe2160deb95c4af2f4c05e3077367c747a
hyeonbae-kim <gusqo14@gmail.com>
2023-06-18 16:55:14 +0900

    inference

--- src/main_infer.py
+++ src/main_infer.py
@@ -92,7 +92,8 @@ class Presumm():
         return predictor
 
     def predict(self, data):
-        texts = [ni.article for ni in data.net_input]
+        # texts = [ni.article for ni in data.net_input]
+        texts = [ni for ni in data]
         input_tokens = []
         sentences_batch = []
         for text in texts:
@@ -127,10 +128,11 @@ class Presumm():
 
         # Get prediction
         def_logger.debug(f'data: {data}, inputs: {inputs}, sentences_batch: {sentences_batch}')
-        if data.extractive:
-            pred = self.predictor["ext"].predict(inputs, sentences_batch, n_sentence=data.top_k)
-        else:
-            pred = self.predictor["abs"].predict(inputs)
+        # if data.extractive:
+            # pred = self.predictor["ext"].predict(inputs, sentences_batch, n_sentence=data.top_k)
+        # else:
+            # pred = self.predictor["abs"].predict(inputs)
+        pred = self.predictor["abs"].predict(inputs)
 
         return Summary(summaries=pred)
 
@@ -139,7 +141,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-visible_gpus', default='-1', type=str)
-    parser.add_argument("-abs_model", default='/content/Summarization_aihub/models/MultiSumAbs_report_512/model_step_20000.pt', type=str)
+    parser.add_argument("-abs_model", default='/home/hyeonbae/project/aihub_github_test/models/MultiSumAbs_report/model_step_100.pt', type=str)
     parser.add_argument("-ext_model", default='', type=str)
     parser.add_argument("-checkpoint_path", default='')
     parser.add_argument("-temp_dir", default='../temp_dir')
@@ -187,7 +189,8 @@ if __name__ == "__main__":
     # k = Kamino(Presumm, local_worker_devices=local_worker_devices, frontdoor_port=3248, queue_port=3249, gather_port=3250,
               #  result_port=3251, skip_port=3252, control_port=3253, port=8001, batch_size=4,
               #  args=args)
-    print(output.predict(data='/content/Summarization_aihub/data/test/report_briefing.test.0.bert.pt'))
+    print(output.predict(data='/home/hyeonbae/project/aihub_github_test/data/test/report_briefing.test.9.bert.pt'))
+    
 
 
     # @k.app.get("/")
